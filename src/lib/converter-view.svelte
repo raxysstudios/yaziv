477a46c9c297cfd8ba65def0bc0d8b192c472660
alkaitagi <alkaitagi@outlook.com>
2022-07-19 22:10:59 +0300

    Fix conversion algorith

--- src/lib/converter-view.svelte
+++ src/lib/converter-view.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import convert from './convert';
-	import type { Converter, Mapping } from './types';
+	import type { Converter, Mapping, Pairs } from './types';
 
 	export let converter: Converter;
-	let mappingFrom: Mapping;
-	let mappingTo: Mapping;
+	let from = converter.mappings[0];
+	let to = converter.mappings[1];
 
-	const pairs = converter.mappings[0].pairs;
+	$: sample = process(converter.sample ?? '', converter.mappings[0]);
+
 	let input = '';
-	$: output = convert(input, pairs);
+	$: output = process(input, from, to);
+
+	function process(input: string, from: Mapping | null = null, to: Mapping | null = null) {
+		let intermediate = input;
+		if (from) intermediate = convert(intermediate, from.pairs);
+		if (to) {
+			const pairs = to.pairs.map((p) => p.slice().reverse()) as Pairs;
+			intermediate = convert(intermediate, pairs);
+		}
+		return intermediate;
+	}
+
+	function reverse() {
+		// const o = input;
+		// const m = mappingFrom;
+		// mappingFrom = mappingTo;
+		// mappingTo = m;
+	}
 </script>
 
 <div
 	class="grid sm:grid-cols-2 grid-rows-[auto_auto_1fr_1fr] sm:grid-rows-[auto_1fr] gap-4 p-4 h-full"
 >
 	<div class="flex flex-row gap-2 h-min">
-		<select class="flex-1 btn" bind:value={mappingFrom}>
+		<select class="flex-1 btn" bind:value={from}>
 			{#each converter.mappings as mapping, i}
 				<option value={mapping}>
 					{i + 1}.
@@ -31,7 +49,7 @@
 		{/if}
 	</div>
 	<div class="flex flex-row gap-2 h-min">
-		<select class="flex-1 btn" bind:value={mappingTo}>
+		<select class="flex-1 btn" bind:value={to}>
 			{#each converter.mappings as mapping, i}
 				<option value={mapping}>
 					{i + 1}.
@@ -42,14 +60,12 @@
 		<div class="btn" on:click={() => navigator.clipboard.writeText(output)}>
 			<Icon icon="ic:round-content-copy" />
 		</div>
+		<div class="btn" on:click={reverse}>
+			<Icon icon="ic:round-swap-horiz" />
+		</div>
 	</div>
-	<textarea class="bg-transparent" bind:value={input} placeholder={converter.sample} />
-	<textarea
-		class="bg-slate-200"
-		value={output}
-		placeholder={convert(converter.sample ?? '', pairs)}
-		readonly
-	/>
+	<textarea class="bg-transparent" bind:value={input} placeholder={process(sample, null, from)} />
+	<textarea class="bg-slate-200" value={output} placeholder={process(sample, null, to)} readonly />
 </div>
 
 <style lang="postcss">
