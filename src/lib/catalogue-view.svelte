b48f3d3128971f6b711698bb35d4abaab5b12031
alkaitagi <alkaitagi@outlook.com>
2022-07-20 01:40:05 +0300

    Reduce catalogue padding

--- src/lib/catalogue-view.svelte
+++ src/lib/catalogue-view.svelte
@@ -3,7 +3,7 @@
 	export let onselect: (a: string) => {};
 </script>
 
-<div class="font-semibold capitalize flex flex-col gap-2 p-4 items-stretch text-center">
+<div class="font-semibold capitalize flex flex-col gap-2 items-stretch text-center">
 	{#each catalogue as item}
 		<p class="btn px-6 pl-10" on:click={() => onselect(item)}>
 			{item}
