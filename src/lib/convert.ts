3fc2f43ea1dfc5920fd98d7d4c3e14255ae0c53f
alkaitagi <alkaitagi@outlook.com>
2022-08-31 19:17:58 +0300

    Fix bug with overlapping whitespaces between pairs

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -1,49 +1,50 @@
-import type { Pairs } from "./types";
+import type { Pairs } from './types';
 
 function capitalize(text: string) {
-  let base = "";
-  let i = 0;
-  if (text.charAt(0) === " ") {
-    base = " ";
-    i = 1;
-  }
-  return base + text.charAt(i).toUpperCase() + text.slice(i + 1);
+	let base = '';
+	let i = 0;
+	if (text.charAt(0) === ' ') {
+		base = ' ';
+		i = 1;
+	}
+	return base + text.charAt(i).toUpperCase() + text.slice(i + 1);
 }
 
 export default function convert(source: string, pairs: Pairs) {
-  source = " " + source.replace(/\n/g, "\n ").trim();
-  let result = "";
+	source = ' ' + source.replace(/\n/g, '\n ').trim();
+	let result = '';
 
-  for (let i = 0; i < source.length;) {
-    let found = false;
-    for (const [from, to] of pairs) {
-      if (!from || !to) continue;
+	for (let i = 0; i < source.length; ) {
+		let found = false;
+		for (const [from, to] of pairs) {
+			if (!from || !to) continue;
 
-      const l = from.length;
-      const sub = source.substring(i, i + l);
-      const pairs = [
-        [from, to],
-        [capitalize(from), capitalize(to)],
-        [from.toUpperCase(), to.toUpperCase()],
-      ];
+			const l = from.length;
+			const sub = source.substring(i, i + l);
+			const pairs = [
+				[from, to],
+				[capitalize(from), capitalize(to)],
+				[from.toUpperCase(), to.toUpperCase()]
+			];
 
-      for (const [f, t] of pairs) {
-        if (sub === f) {
-          found = true;
-          result += t;
-          i += l;
-          break;
-        }
-      }
+			for (const [f, t] of pairs) {
+				if (sub === f) {
+					found = true;
+					result += t;
+					i += l;
+					if (t && t[t.length - 1] === ' ') i--;
+					break;
+				}
+			}
 
-      if (found) break;
-    }
+			if (found) break;
+		}
 
-    if (!found) {
-      result += source[i];
-      i++;
-    }
-  }
+		if (!found) {
+			result += source[i];
+			i++;
+		}
+	}
 
-  return result.replace(/\n /g, "\n").trim();
+	return result.replace(/\n /g, '\n').trim();
 }
