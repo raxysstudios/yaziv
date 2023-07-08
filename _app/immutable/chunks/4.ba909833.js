85df2a832a98e8de1897d24c3f8ad7c6bf0e5e49
alkaitagi <alkaitagi@outlook.com>
2023-07-08 19:23:08 +0000

    Deploying to gh-pages from @ raxysstudios/yaziv@d0822a951f293123b81e06fe6d4f4321b759ce9b 🚀

---
+++ _app/immutable/chunks/4.ba909833.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/menu-page.svelte.0e005698.js";export{t as component};
--- _app/immutable/chunks/4.ddcbdf6d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/menu-page.svelte.81892a40.js";export{t as component};
