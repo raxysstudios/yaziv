6410fd169e81b662f4a35f8168e8c2f2dfa9600a
alkaitagi <alkaitagi@outlook.com>
2023-07-10 14:39:25 +0300

    Fix URL

--- src/lib/voice.ts
+++ src/lib/voice.ts
@@ -3,8 +3,9 @@ import { client } from '@gradio/client';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let app: any;
 export async function predict(iso: string, input: string) {
-	app ??= await client('https://mms-meta-mms--rt5v2.hf.space/');
+	const base = 'https://mms-meta-mms--5jtk9.hf.space/';
+	app ??= await client(base);
 	const result = await app.predict('/predict_1', [input, iso, 0.9]);
 	const url = result.data[0].value.name;
-	return 'https://mms-meta-mms--rt5v2.hf.space/file=' + url;
+	return base + 'file=' + url;
 }
