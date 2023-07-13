f5a60433ce12b0fc10f117d0e2802d6d4e793419
alkaitagi <alkaitagi@outlook.com>
2023-07-13 11:49:49 +0300

    Fix url

--- src/lib/voice.ts
+++ src/lib/voice.ts
@@ -3,7 +3,7 @@ import { client } from '@gradio/client';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let app: any;
 export async function predict(iso: string, input: string) {
-	const base = 'https://mms-meta-mms--5jtk9.hf.space/';
+	const base = 'https://mms-meta-mms--dzmwz.hf.space/';
 	app ??= await client(base);
 	const result = await app.predict('/predict_1', [input, iso, 0.9]);
 	const url = result.data[0].value.name;
