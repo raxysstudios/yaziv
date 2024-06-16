d66b4b8d60bacf7f647eef32bd7a4c8ae96e241c
alkaitagi <alkaitagi@outlook.com>
2024-06-16 20:16:12 +0300

    Sort allLangs

--- composables/lang.ts
+++ composables/lang.ts
@@ -1,50 +1,46 @@
-import { onMounted } from 'vue'
-import type { Lang } from '~/utils/types';
+import type { Lang } from "~/utils/types";
 
 const allLangs = [
   {
-    "id": "abq",
-    "name": "abaza"
+    id: "abq",
+    name: "abaza",
   },
   {
-    "id": "abk",
-    "name": "abkhaz"
+    id: "abk",
+    name: "abkhaz",
   },
   {
-    "id": "agx",
-    "name": "aghul"
+    id: "agx",
+    name: "aghul",
   },
   {
-    "id": "xdq",
-    "name": "kaitag"
+    id: "xdq",
+    name: "kaitag",
   },
   {
-    "id": "kum",
-    "name": "kumyk"
+    id: "kum",
+    name: "kumyk",
   },
   {
-    "id": "tkr",
-    "name": "tsakhur"
+    id: "tkr",
+    name: "tsakhur",
   },
   {
-    "id": "oss",
-    "name": "ossetian",
-    "flag": "iron"
+    id: "oss",
+    name: "ossetian",
+    flag: "iron",
   },
   {
-    "id": "lez",
-    "name": "lezgi"
-  }
+    id: "lez",
+    name: "lezgi",
+  },
 ] as Lang[];
 
-export default async function useLang() {
-  const lang = useState('lang', () => '');
+export default function useLang() {
+  const lang = useState("lang", () => "");
   watch(lang, (val) => {
-    localStorage.setItem('lang', val);
+    localStorage.setItem("lang", val);
   });
-  onMounted(async () => {
-    lang.value = localStorage.getItem('lang') ?? '';
-  });
-
+  allLangs.sort((a, b) => a.name.localeCompare(b.name));
   return { lang, allLangs };
 }
