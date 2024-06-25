dfe48e9bdde2aa54bb03f4de1c6f7ca4d3ab556f
alkaitagi <alkaitagi@outlook.com>
2024-06-25 16:51:50 +0300

    Bring back flags

--- components/lang-card.vue
+++ components/lang-card.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
 import type { Lang } from "~/utils/types";
 
-defineProps<{
+const props = defineProps<{
     lang: Lang
 }>();
 
-// const flagUrl = computed(() => {
-//     const base = 'https://firebasestorage.googleapis.com/v0/b/avzagapp.appspot.com/o/flags%2F';
-//     const flag = props.lang.flag ?? props.lang.name.toLowerCase();
-//     return `${base}${flag}.png?alt=media`;
-// });
+const flagUrl = computed(() => {
+    const base = 'https://firebasestorage.googleapis.com/v0/b/avzagapp.appspot.com/o/flags%2F';
+    const flag = props.lang.flag ?? props.lang.name.toLowerCase();
+    return `${base}${flag}.png?alt=media`;
+});
 </script>
 
 <template>
     <NuxtLink :to="`/?lang=${lang.id}`" class="work-p group">
         <div class="work-c flex gap-4 items-center">
-            <!-- <UAvatar class="ring-1 ring-slate-200" size="xs" img-class="object-cover"
-                :alt="lang.id" /> -->
-            <UButton style='solid' size="2xs" color="gray">
-                {{ lang.id }}
-            </UButton>
+            <UAvatar :src="flagUrl" class="ring-1 ring-slate-200" size="xs" img-class="object-cover" :alt="lang.id" />
             <p class="capitalize">{{ lang.name }}</p>
             <UIcon class="text-sm opacity-0 -translate-x-2 group-hover:translate-x-0 group-hover:opacity-100"
                 name="i-heroicons-arrow-right" />
