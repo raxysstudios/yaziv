a8e57ddcda5644dccc0e3c958b92d054bf35e5b4
alkaitagi <alkaitagi@outlook.com>
2024-06-25 17:36:17 +0300

    Clean up components

--- components/app-header.vue
+++ components/app-header.vue
@@ -9,15 +9,10 @@ defineProps<{
 <template>
     <NuxtLink :to="link" class="work-p">
         <div class="work-c flex items-center gap-4 text-lg capitalize font-medium">
-            <!-- <UIcon class="text-sm" name="i-heroicons-arrow-left" />
-            {{ title }}
-            <UButton variant="solid" size="2xs" color="gray" class="capitalize">
-                {{ lang }}
-            </UButton> -->
             <UIcon v-if="icon" :name="icon" />
             <span class="capitalize font-medium">{{ title }}</span>
             <div class="flex-1" />
-            <div @click="(e) => e.stopPropagation()">
+            <div @click="(e: any) => e.stopPropagation()">
                 <slot />
             </div>
         </div>
