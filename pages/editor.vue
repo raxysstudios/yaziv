a30963feed4deec30f1d688fcd2472358ad07433
alkaitagi <alkaitagi@outlook.com>
2024-06-27 14:42:40 +0300

    Fix editor back link

--- pages/editor.vue
+++ pages/editor.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="w-full flex flex-col items-center">
-        <AppHeader to="/menu" badge="[EDITOR]" icon="i-heroicons-arrow-left" />
-        <UDivider />
+        <AppHeader link="/menu" badge="[EDITOR]" icon="i-heroicons-bars-3" />
         <div class="work-c flex flex-col gap-2 my-2 items-stretch">
             Under construction
         </div>
