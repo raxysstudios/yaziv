815b9d7e918d698389c5c78362bd1944cee491e0
alkaitagi <alkaitagi@outlook.com>
2025-08-10 06:52:57 +0300

    Install icons module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", '@nuxt/fonts'],
+  modules: ["@nuxt/ui", '@nuxt/fonts', '@nuxt/icon'],
   colorMode: {
     preference: 'light'
   },
