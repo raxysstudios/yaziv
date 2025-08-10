d0a9cc49face7bb240f61f1382a44aa4cef64f04
alkaitagi <alkaitagi@outlook.com>
2025-08-10 14:21:06 +0300

    Elevate the app bar

--- app/components/scaffold.vue
+++ app/components/scaffold.vue
@@ -9,22 +9,21 @@ defineProps<{
 </script>
 
 <template>
-    <div id="work" class="w-full flex flex-col items-center p-2 gap-2">
-        <div class="w-full sm:w-5/6 lg:w-2/3 flex flex-col gap-2">
-            <div class="flex flex-row items-center gap-2">
-                <NuxtLink :to="link">
-                    <UButton :icon="icon ?? 'i-material-symbols-arrow-back'" />
-                </NuxtLink>
-                <p class="text-lg capitalize font-medium ">Yaziv</p>
-                <UBadge v-if="badge" class="capitalize" variant="soft">
-                    {{ badge }}
-                </UBadge>
-                <div class="flex-1" />
-                <slot name="actions" />
-            </div>
-
+    <div id="work" class="w-full shadow flex flex-col items-center p-2 gap-2">
+        <div class="w-full sm:w-5/6 lg:w-2/3 flex flex-row items-center gap-2">
+            <NuxtLink :to="link">
+                <UButton :icon="icon ?? 'i-material-symbols-arrow-back'" />
+            </NuxtLink>
+            <p class="text-lg capitalize font-medium ">Yaziv</p>
+            <UBadge v-if="badge" class="capitalize" variant="soft">
+                {{ badge }}
+            </UBadge>
+            <div class="flex-1" />
+            <slot name="actions" />
         </div>
-        <USeparator class="w-[calc(100%+var(--spacing)*4)]" />
+    </div>
+    <USeparator class="w-full" />
+    <div id="work" class="w-full flex flex-col items-center p-2 gap-2">
         <div class="w-full sm:w-5/6 lg:w-2/3 flex flex-col gap-2">
             <slot />
         </div>
