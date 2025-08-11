6cb93ec94d3bf79d39d62f48b5bd3ea94cce8a61
alkaitagi <alkaitagi@outlook.com>
2025-08-11 06:22:31 +0300

    Clean up

--- app/middleware/redirect-root.ts
+++ app/middleware/redirect-root.ts
@@ -1,5 +1,4 @@
 export default defineNuxtRouteMiddleware((to) => {
-    console.log('MIDDLE');
     if (import.meta.client && to.path === '/') {
         const lastUrl = localStorage.getItem('lastUrl');
         if (lastUrl && lastUrl !== '/') {
