806a1d1da74aebbb5aac26bf59c7ebe4cb0ada43
alkaitagi <alkaitagi@outlook.com>
2025-08-13 23:51:34 +0300

    Add home footer

--- app/components/HomeScreen.vue
+++ app/components/HomeScreen.vue
@@ -22,13 +22,19 @@ useSeoMeta({
                 <UButton icon="i-material-symbols-edit-square-outline" />
             </UTooltip>
         </NuxtLinkLocale>
-        <NuxtLinkLocale to="https://github.com/raxysstudios/yaziv" target="_blank">
-            <UTooltip :delay-duration="0" :text="$t('home.github')">
-                <UButton icon="i-material-symbols-code" />
-            </UTooltip>
-        </NuxtLinkLocale>
     </AppHeader>
     <NuxtLinkLocale v-for="l in allLangs" :key="l.id" :to="`/${l.id}`">
         <LanguageSegment :lang="l" />
     </NuxtLinkLocale>
+    <AppSegment>
+        <USeparator></USeparator>
+        <div class="flex justify-between font-bold!">
+            <UButton to="https://raxys.app" target="_blank" icon="i-material-symbols-landscape" size="xs">
+                {{ $t('home.raxys') }}
+            </UButton>
+            <UButton to="https://github.com/raxysstudios/yaziv" target="_blank" size="xs">
+                v1.2.1
+            </UButton>
+        </div>
+    </AppSegment>
 </template>
