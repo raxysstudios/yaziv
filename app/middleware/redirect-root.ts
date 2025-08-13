5051da4485153a621bdce009bd7f7d754125f2ae
alkaitagi <alkaitagi@outlook.com>
2025-08-13 20:54:26 +0300

    Revert no-slash

--- app/middleware/redirect-root.ts
+++ app/middleware/redirect-root.ts
@@ -1,12 +1,12 @@
 export default defineNuxtRouteMiddleware((to) => {
     if (!import.meta.client) return;
 
-    let lastUrl = localStorage.getItem('lastUrl');
+    const lastUrl = localStorage.getItem('lastUrl');
     if (lastUrl && lastUrl !== '/') {
-        // Temporary fix to migrate from old saves
-        if (lastUrl.endsWith('/')) {
-            lastUrl = lastUrl.slice(0, -1);
-        }
+        // // Temporary fix to migrate from old saves
+        // if (lastUrl.endsWith('/')) {
+        //     lastUrl = lastUrl.slice(0, -1);
+        // }
         return navigateTo(lastUrl);
     }
     return navigateTo('/home');
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   },
   router: {
     options: {
-      strict: true
+      // strict: true
     }
   },
   app: {
