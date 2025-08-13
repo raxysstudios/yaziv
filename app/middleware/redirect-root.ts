9cafe6befec46205d05862c7adde8cf5aa8b253e
alkaitagi <alkaitagi@outlook.com>
2025-08-13 20:35:14 +0300

    Add temp fix

--- app/middleware/redirect-root.ts
+++ app/middleware/redirect-root.ts
@@ -1,8 +1,12 @@
 export default defineNuxtRouteMiddleware((to) => {
     if (!import.meta.client) return;
 
-    const lastUrl = localStorage.getItem('lastUrl');
+    let lastUrl = localStorage.getItem('lastUrl');
     if (lastUrl && lastUrl !== '/') {
+        // Temporary fix to migrate from old saves
+        if (lastUrl.endsWith('/')) {
+            lastUrl = lastUrl.slice(0, -1);
+        }
         return navigateTo(lastUrl);
     }
     return navigateTo('/home');
