de7745b5686227aeacb543cddd7eec5bf5594c65
alkaitagi <alkaitagi@outlook.com>
2025-08-16 15:15:33 +0300

    Move footer into a component

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <AppSegment>
+        <USeparator />
+        <div class="flex justify-between font-bold!">
+            <UButton to="https://raxys.app" target="_blank" icon="i-material-symbols-landscape" size="xs">
+                {{ $t('home.raxys') }}
+            </UButton>
+            <UButton to="https://github.com/raxysstudios/yaziv" target="_blank" size="xs">
+                v1.2.2
+            </UButton>
+        </div>
+    </AppSegment>
+</template>
--- app/components/HomeScreen.vue
+++ app/components/HomeScreen.vue
@@ -21,15 +21,5 @@ useSeoMeta({
     <NuxtLinkLocale v-for="l in langs" :key="l.id" :to="`/${l.id}`">
         <LanguageSegment :lang="l" />
     </NuxtLinkLocale>
-    <AppSegment>
-        <USeparator />
-        <div class="flex justify-between font-bold!">
-            <UButton to="https://raxys.app" target="_blank" icon="i-material-symbols-landscape" size="xs">
-                {{ $t('home.raxys') }}
-            </UButton>
-            <UButton to="https://github.com/raxysstudios/yaziv" target="_blank" size="xs">
-                v1.2.1
-            </UButton>
-        </div>
-    </AppSegment>
+    <AppFooter />
 </template>
