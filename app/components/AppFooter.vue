a430d35be058aa06e31e627fbbc5ffd9f76f9598
alkaitagi <alkaitagi@outlook.com>
2025-08-16 16:31:47 +0300

    Bump

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -6,7 +6,7 @@
                 {{ $t('home.raxys') }}
             </UButton>
             <UButton to="https://github.com/raxysstudios/yaziv" target="_blank" size="xs">
-                v1.2.2
+                v1.2.4
             </UButton>
         </div>
     </AppSegment>
