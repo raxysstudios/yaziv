518eb04b892e101dd2f3ddaedab3abbf06346944
Mæhæmad <alkaitagi@outlook.com>
2025-09-21 23:02:00 +0000

    Change editor icon

--- app/components/HomeScreen.vue
+++ app/components/HomeScreen.vue
@@ -14,7 +14,7 @@ useSeoMeta({
     <AppHeader :link="lastUrl ?? undefined">
         <NuxtLinkLocale to="/editor">
             <UTooltip :delay-duration="0" :text="$t('home.editor')">
-                <UButton icon="i-material-symbols-tools-wrench-rounded" />
+                <UButton icon="i-material-symbols-table-edit-rounded" />
             </UTooltip>
         </NuxtLinkLocale>
     </AppHeader>
