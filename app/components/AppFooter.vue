23ede2f44337a018464751eb0d545d9b92190b9a
Mæhæmad <alkaitagi@outlook.com>
2025-09-21 17:01:50 +0000

    Bump

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -6,7 +6,7 @@
                 {{ $t('home.raxys') }}
             </UButton>
             <UButton to="https://github.com/raxysstudios/yaziv" target="_blank" size="xs">
-                v1.2.5
+                v1.3.0
             </UButton>
         </div>
     </AppSegment>
