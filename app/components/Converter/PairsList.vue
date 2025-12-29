e78d61d6f3bcace3a32ded7168040b7d77e182cb
Mæhæmad <alkaitagi@outlook.com>
2025-12-29 13:59:53 +0000

    WIP fix the pairs list

--- app/components/Converter/PairsList.vue
+++ app/components/Converter/PairsList.vue
@@ -1,37 +1,34 @@
 <script setup lang="ts">
-import type { Mapping, Pairs } from '~/utils/types';
+import type { Mapping } from '~/utils/types';
 
 const props = defineProps<{
-    from: Mapping
-    to: Mapping
-    convert: (text: string) => string
+  from: Mapping
+  to: Mapping
+  convert: (text: string) => string
 }>()
 
 const mergedPairs = computed(() => {
-    let pairs = [] as Pairs;
-    if (props.from.pairs.length) {
-        pairs = props.from.pairs.map(([a, b]) => [
-            a,
-            props.convert(b)
-        ]);
-    } else if (props.to.pairs.length) {
-        pairs = props.to.pairs.map(([a, b]) => [b, a]);
-    }
-    return pairs.filter(([a, b]) => a !== b);
-}
-);
+  let pairs = [] as [string, string][];
+  if (props.from.pairs.length) {
+    pairs = props.from.pairs
+      .map(([a]) => [a, props.convert(a)]);
+  } else if (props.to.pairs.length) {
+    pairs = props.to.pairs
+      .filter(([, , ct]) => ct != '>')
+      .map(([a, b]) => [b, a]);
+  }
+  return pairs.map(
+    p => p.map(e => e.replaceAll(' ', '·'))
+  );
+});
 </script>
 
 <template>
-    <div class="flex native justify-center flex-row flex-wrap gap-1 text-sm">
-        <div v-for="p in mergedPairs" class="flex px-1.5 rounded-sm bg-elevated/75">
-            <span class="text-dimmed">
-                {{ p[0].replaceAll(' ', '·') }}
-            </span>
-            <span>&nbsp;</span>
-            <span>
-                {{ p[1].replaceAll(' ', '·') }}
-            </span>
-        </div>
+  <div class="flex native justify-center flex-row flex-wrap gap-1 text-sm">
+    <div v-for="p in mergedPairs" class="flex px-1.5 rounded-sm bg-elevated/75">
+      <span class="text-dimmed">{{ p[0] }}</span>
+      <span>&nbsp;</span>
+      <span>{{ p[1] }}</span>
     </div>
+  </div>
 </template>
